9e6641691a3b6a0a96e32e050d5a2e508507a26a
paulich07 <50242426+paulich07@users.noreply.github.com>
2025-11-17 18:21:10 +0100

    fixed clean

--- src/program_utils.c
+++ src/program_utils.c
@@ -6,7 +6,7 @@
 /*   By: plichota <plichota@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 17:20:24 by plichota          #+#    #+#             */
-/*   Updated: 2025/11/17 18:01:09 by plichota         ###   ########.fr       */
+/*   Updated: 2025/11/17 18:20:40 by plichota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 // distrugge e libera window
 void	exit_program(t_window *win, char *s, int error)
 {
-
 	// if (win->img_floor)
 	// 	mlx_destroy_image(win->mlx, win->img_floor);
 	// if (win->img_wall)
@@ -29,15 +28,18 @@ void	exit_program(t_window *win, char *s, int error)
 	// 	mlx_destroy_image(win->mlx, win->img_collect);
 	// if (win->img_exit)
 	// 	mlx_destroy_image(win->mlx, win->img_exit);
+	if (win->map)
+		ft_free_mtx(win->map);
 	if (win->win_img)
 	{
 		// mlx_destroy_image frees also addr
-		mlx_destroy_image(win->mlx, win->win_img->img);
+		if (win->win_img->img)
+			mlx_destroy_image(win->mlx, win->win_img->img);
 		free(win->win_img);
+		win->win_img = NULL;
 	}
 	if (win->win)
 		mlx_destroy_window(win->mlx, win->win);
-	free(win);
 	if (win->mlx)
 	{
 		mlx_destroy_display(win->mlx);
